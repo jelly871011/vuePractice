83b5c10b4347bd8b4c2fd4bdeae69b8ce6c63f2d
jelly <jellylu@funtopiagames.com>
2024-02-27 14:16:12 +0800

    加回移除的檔案 & 新增gitignore

---
+++ .eslintrc.cjs
@@ -0,0 +1,59 @@
+/* eslint-disable no-tabs */
+
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:vue/vue3-essential',
+		'airbnb-base',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'@vue/eslint-config-airbnb',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [
+				'.eslintrc.{js,cjs}',
+			],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		parser: '@typescript-eslint/parser',
+		sourceType: 'module',
+	},
+	plugins: [
+		'@typescript-eslint',
+		'vue',
+	],
+	rules: {
+		'vue/component-definition-name-casing': [
+			'off',
+		],
+		'no-tabs': 'off',
+		indent: [
+			'error',
+			'tab',
+		],
+		quotes: [
+			'error',
+			'single',
+		],
+		semi: [
+			'error',
+			'always',
+		],
+		'vue/no-multiple-template-root': [
+			'off',
+		],
+		'no-console': 'off',
+	},
+};
